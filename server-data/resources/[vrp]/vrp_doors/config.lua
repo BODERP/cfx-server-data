ece5eb7ee688b9344ebb4ecd46c9bba8771a2cf2
Daniel <daniel@becher.com.br>
2024-01-13 21:47:37 -0300

    trocando a base

---
+++ server-data/resources/[vrp]/vrp_doors/config.lua
@@ -0,0 +1,85 @@
+local cfg = {}
+
+cfg.list = {
+	------"PORTAS POLÍCIA DP PRAÇA"------
+	[1] = { text = true, hash = 631614199, ['x'] = 461.86, ['y'] = -993.68, ['z'] = 24.91, lock = true, perm = "portadp.permissao" }, -- Celas
+	[2] = { text = true, hash = 631614199, ['x'] = 461.84, ['y'] = -998.37, ['z'] = 24.91, lock = true, perm = "portadp.permissao" }, -- Celas
+	[3] = { text = true, hash = 631614199, ['x'] = 461.83, ['y'] = -1002.08, ['z'] = 24.91, lock = true, perm = "portadp.permissao" }, -- Celas
+	[4] = { text = true, hash = 2271212864, ['x'] = 468.29, ['y'] = -1014.15, ['z'] = 26.38, lock = true, perm = "portadp.permissao", other = 5 }, -- Porta de Trás da DP E
+	[5] = { text = true, hash = 2271212864, ['x'] = 468.96, ['y'] = -1014.09, ['z'] = 26.38, lock = true, perm = "portadp.permissao", other = 4 }, -- Porta de Trás da DP D
+	[6] = { text = true, hash = 185711165, ['x'] = 445.37, ['y'] = -989.77, ['z'] = 30.68, lock = true, perm = "portadp.permissao", other = 7 }, -- Porta de Trás da DP D
+	[7] = { text = true, hash = 185711165, ['x'] = 444.07, ['y'] = -989.77, ['z'] = 30.68, lock = true, perm = "portadp.permissao", other = 6 }, -- Porta de Trás da DP E
+	[8] = { text = true, hash = 749848321, ['x'] = 453.07, ['y'] = -982.68, ['z'] = 30.68, lock = true, perm = "portadp.permissao" }, -- Arsenal PM
+	[9] = { text = true, hash = 1557126584, ['x'] = 450.05, ['y'] = -986.75, ['z'] = 30.68, lock = true, perm = "portadp.permissao" }, -- Arsenl PM
+
+	------"PORTAS CASAS PADRÃO DO GTA"------
+	[10] = { text = true, hash = 520341586, ['x'] = -14.12, ['y'] = -1441.47, ['z'] = 31.10, lock = true, perm = "admin.permissao" }, -- Casa Antiga do Franklin
+	[11] = { text = true, hash = 3687927243, ['x'] = -1150.05, ['y'] = -1521.69, ['z'] = 10.62, lock = true, perm = "admin.permissao" }, -- Casa Praia do Trevor
+	[12] = { text = true, hash = 486670049, ['x'] = -106.93, ['y'] = -8.45, ['z'] = 70.52, lock = true, perm = "admin.permissao" }, -- Apartamento LS Custom Centro
+	[13] = { text = true, hash = 308207762, ['x'] = 8.22, ['y'] = 539.46, ['z'] = 176.02, lock = true, perm = "admin.permissao" }, -- Mansão do Franklin
+	[14] = { text = true, hash = 159994461, ['x'] = -816.94, ['y'] = 178.13, ['z'] = 72.22, lock = true, permn = "admin.permissao", other = 22 }, -- Mansão do Michael
+    [15] = { text = true, hash = 132154435, ['x'] = 1973.54, ['y'] = 3815.41, ['z'] = 33.42, lock = true, perm = "admin.permissao" }, -- Trailer Trevor
+	[16] = { text = true, hash = 2264746914, ['x'] = -1002.85, ['y'] = -477.84, ['z'] = 50.02, lock = true, perm = "admin.permissao" }, -- Escritório do Prefeito/Lavagem
+	--[17] = { text = false, hash = 1282049587, ['x'] = 3568.52, ['y'] = 3693.98, ['z'] = 28.12, lock = true, perm = "humane.permissao", other = 27 },
+	--[18] = { text = false, hash = 3873258242, ['x'] = 3568.74, ['y'] = 3695.23, ['z'] = 28.12, lock = true, perm = "humane.permissao", other = 26 },
+	--[19] = { text = false, hash = 1282049587, ['x'] = 3597.23, ['y'] = 3689.92, ['z'] = 28.83, lock = true, perm = "humane.permissao", other = 29 },
+	--[20] = { text = false, hash = 3873258242, ['x'] = 3597.7, ['y'] = 3689.61, ['z'] = 28.83, lock = true, perm = "humane.permissao", other = 28 },
+	--[21] = { text = false, hash = 1388116908, ['x'] = 552.33, ['y'] = -3118.09, ['z'] = 18.76, lock = true, perm = "italianos.permissao" },
+	--[22] = { text = false, hash = 1388116908, ['x'] = 585.92, ['y'] = -3118.13, ['z'] = 18.76, lock = true, perm = "italianos.permissao" },
+    --[23] = { text = false, hash = 1335311341, ['x'] = 1187.96, ['y'] = 2644.97, ['z'] = 38.40, lock = true, perm = "corleone.permissao" },
+	[24] = { text = true, hash = 190770132, ['x'] = 981.93, ['y'] = -102.54, ['z'] = 74.85, lock = true, perm = "motoclub.permissao" },
+	[25] = { text = true, hash = -1116041313, ['x'] = 127.95, ['y'] = -1298.50, ['z'] = 29.41, lock = true, perm = "vanilla.permissao", others = 35 },
+	[26] = { text = true, hash = 668467214, ['x'] = 96.09, ['y'] =  -1284.85, ['z'] = 29.43, lock = true, perm = "vanilla.permissao", others = 34 },
+	[27] = { text = true, hash = 2739859149, ['x'] = -1607.47, ['y'] = -3006.47, ['z'] = -75.2, lock = true, perm = "bratva.permissao" },
+	[28] = { text = true, hash = 2305201762, ['x'] = -1607.44, ['y'] = -3006.47, ['z'] = -79.0, lock = true, perm = "bratva.permissao" },
+	[29] = { text = true, hash = 3652358431, ['x'] = -2065.00, ['y'] = -456.33,  ['z'] = 16.43, lock = true, perm = "policia.permissao" },
+	[30] = { text = true, hash = 3652358431, ['x'] = -2061.24, ['y'] =-459.16,   ['z'] = 16.43, lock = true, perm = "policia.permissao" },
+	--[31] = { text = true, hash = -991486064, x = 84.66, y = -1959.76, z = 21.12, lock = true, perm = "desmanche.permissao" },
+	--[32] = { text = true, hash = -1163592260, x = 75.639, y = -1969.70, z = 21.12, lock = true, perm = "desmanche.permissao" },
+	--[33] = { text = true, hash = -1563799200, x = 74.70, y = -1966.69, z = 21.12, lock = true, perm = "desmanche.permissao" }
+	
+	--portas do farm de armas
+	[34] = { text = true, hash = 362975687, ['x'] = -623.66, ['y'] = -1624.12, ['z'] = 33.02, lock = true, perm = "reciclagem.permissao" },--Porta de Acesso ao Facção de Armas recepção - esquerda
+	[35] = { text = true, hash = 362975687, ['x'] = -624.89, ['y'] = -1624.05, ['z'] = 33.02, lock = true, perm = "reciclagem.permissao" },--POrta de Acesso à Facção de Armas recepção - direita
+	[36] = { text = true, hash = 1099436502, ['x'] = -592.42, ['y'] = -1630.89, ['z'] = 26.99, lock = true, perm = "reciclagem.permissao" },--POrta de trás - esquerda
+	[37] = { text = true, hash = -1627599682, ['x'] = -592.46, ['y'] = -1629.61, ['z'] = 26.99, lock = true, perm = "reciclagem.permissao" },--POrta de trás - direita
+	[38] = { text = true, hash = 362975687, ['x'] = -604.98, ['y'] = -1626.99, ['z'] = 27.02, lock = true, perm = "reciclagem.permissao" },
+	[39] = { text = true, hash = 362975687, ['x'] = -606.19, ['y'] = -1626.76, ['z'] = 27.02, lock = true, perm = "reciclagem.permissao" },
+	[40] = { text = true, hash = 362975687, ['x'] = -603.63, ['y'] = -1611.89, ['z'] = 27.02, lock = true, perm = "reciclagem.permissao" },
+	[41] = { text = true, hash = 362975687, ['x'] = -604.91, ['y'] = -1611.75, ['z'] = 27.02, lock = true, perm = "reciclagem.permissao" },
+	[42] = { text = true, hash = 362975687, ['x'] = -568.04, ['y'] = -1601.74, ['z'] = 27.02, lock = true, perm = "reciclagem.permissao" },
+	[43] = { text = true, hash = 362975687, ['x'] = -567.92, ['y'] = -1600.55, ['z'] = 27.02, lock = true, perm = "reciclagem.permissao" },
+	[44] = { text = true, hash = 362975687, ['x'] = -561.46, ['y'] = -1620.84, ['z'] = 27.02, lock = true, perm = "reciclagem.permissao" },
+	[45] = { text = true, hash = 362975687, ['x'] = -562.58, ['y'] = -1620.87, ['z'] = 27.02, lock = true, perm = "reciclagem.permissao" },
+	[46] = { text = true, hash = 362975687, ['x'] = -566.73, ['y'] = -1629.81, ['z'] = 33.02, lock = true, perm = "reciclagem.permissao" },
+	[47] = { text = true, hash = 362975687, ['x'] = -566.59, ['y'] = -1628.6, ['z'] = 33.02, lock = true, perm = "reciclagem.permissao" }, 
+	[48] = { text = true, hash = -1156020871, ['x'] = -552.82, ['y'] = -1598.24, ['z'] = 18.83, lock = true, perm = "reciclagem.permissao" }, -- canil direita
+	[49] = { text = true, hash = -1156020871, ['x'] = -551.02, ['y'] = -1598.46, ['z'] = 18.83, lock = true, perm = "reciclagem.permissao" }, -- canil meio
+	[50] = { text = true, hash = -1156020871, ['x'] = -549.26, ['y'] = -1598.59, ['z'] = 18.88, lock = true, perm = "reciclagem.permissao" }, -- canil esquerda
+	
+	-- Portas Celas Embaixo da DP
+	[51] = { text = true, hash = -147325430, ['x'] = 97.62, ['y'] = 6328.11, ['z'] = 31.37, lock = true, perm = "helipa.permissao" },
+	[52] = { text = true, hash = 1335311341, ['x'] = 959.01, ['y'] = -121.41, ['z'] = 75.09, lock = true, perm = "motoclub.permissao" },
+	--[53] = { text = true, hash = -1653461382, ['x'] = -948.87, ['y'] = -1476.78, ['z'] = 6.8, lock = true, perm = "lifeinvader.permissao", other = 62},
+	--[54] = { text = false, hash = 1504256620, ['x'] = -1002.85, ['y'] = -477.84, ['z'] = 50.02, lock = true, perm = "admin.permissao",  other = 66 },
+	--[55] = { text = false, hash = 262671971, ['x'] = -1002.85, ['y'] = -477.84, ['z'] = 50.02, lock = true, perm = "admin.permissao",  other = 65 },
+	--[56] = { text = false, hash = 262671971, ['x'] = -1002.85, ['y'] = -477.84, ['z'] = 50.02, lock = true, perm = "admin.permissao"},
+	--[57] = { text = true, hash = -1249591818, ['x'] = -2652.87, ['y'] = 1326.35, ['z'] = 147.45, lock = true, perm = "mafia.permissao" },
+	[58] = { text = true, hash = -1563640173, ['x'] = -1500.78, ['y'] = 103.61, ['z'] = 55.68, lock = true, perm = "admin.permissao" },
+	[59] = { text = true, hash = -1563640173, ['x'] = -1522.0, ['y'] = 143.38, ['z'] = 55.66, lock = true, perm = "admin.permissao" },
+	[60] = { text = true, hash = 1033441082, ['x'] = -1537.02, ['y'] = 130.74, ['z'] = 57.38, lock = true, perm = "admin.permissao" },
+	--[61] = { text = true, hash = 1099436502, ['x'] = -592.44, ['y'] = -1630.9, ['z'] = 26.99, lock = true, perm = "flanela.permissao", others = 70 },
+	--[62] = { text = true, hash = -1627599682, ['x'] = -592.32, ['y'] = -1629.6, ['z'] = 26.99, lock = true, perm = "flanela.permissao", others = 69 },
+	--[63] = { text = true, hash = -1627599682, ['x'] = -592.32, ['y'] = -1629.6, ['z'] = 26.99, lock = true, perm = "instrutor.permissao", others = 72 },
+	--[64] = { text = true, hash = -1627599682, ['x'] = -592.32, ['y'] = -1629.6, ['z'] = 26.99, lock = true, perm = "instrutor.permissao", others = 71 },
+	[65] = { text = true, hash = 1173348778, ['x'] = -1482.17, ['y'] = -375.54, ['z'] = 40.17, lock = true, perm = "contrabandista.permissao"}, -- 1173176141 hash do Codewalker salvando
+	
+	-- Portas da importação da pasta base de cocaína
+	[66] = { text = true, hash = 464151082, ['x'] = 133.83, ['y'] = -2203.42, ['z'] = 7.19, lock = true, perm = "campinho.permissao"}, -- porta da pasta base no porto.
+	[67] = { text = true, hash = 1173348778, ['x'] = -934.82, ['y'] = -2927.0, ['z'] = 13.96, lock = true, perm = "campinho.permissao"}, -- porta externa do hangar
+	[68] = { text = true, hash = -1775213343, ['x'] = -933.33, ['y'] = -2934.26, ['z'] = 13.95, lock = true, perm = "campinho.permissao"}, -- porta interna do hangar direita
+	[69] = { text = true, hash = -1775213343, ['x'] = -932.9, ['y'] = -2933.7, ['z'] = 13.95, lock = true, perm = "campinho.permissao"} -- porta interna do hangar esquerda
+	
+}
+
+return cfg
